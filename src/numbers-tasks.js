2548951c881db761b3ae65a0c0063866a5726635
MaxMutny <buika190889@gmail.com>
2023-12-21 21:52:32 +0100

    task_1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,6 +7,7 @@
  *                                                                                           *
  ******************************************************************************************* */
 
+/* eslint-disable */
 /**
  * Returns an area of a rectangle given by width and height.
  *
@@ -18,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height ) {
+  return (width * height);
 }
 
 /**
