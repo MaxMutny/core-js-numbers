816c503560b664f1c52b0257963a574dd99de4f3
MaxMutny <buika190889@gmail.com>
2023-12-24 22:58:51 +0100

    done567

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -87,8 +87,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2 ) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot( a, b ) {
+  return(- b / a);
 }
 
 /**
@@ -108,8 +108,10 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  let scalar = (x1 * x2) + (y1 * y2);
+  let modul = (((x1 **2) + (y1 ** 2)) ** -2) * (((x2 **2) + (y2 ** 2)) ** -2);
+  return scalar / modul
 }
 
 /**
@@ -125,8 +127,12 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  let arrayValueDigit = Array.from(String(value), Number);
+  let lasrNumber = arrayValueDigit.pop();
+   
+
+  return (lasrNumber);
 }
 
 /**
@@ -140,8 +146,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+ 
+  return(Number(value));
 }
 
 /**
@@ -157,8 +164,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return (((a ** 2) + (b ** 2) + (c ** 2)) ** - 2);
 }
 
 /**
@@ -199,8 +206,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n % 2 === 0) {
+    return false 
+  }
+  else 
+  return true 
 }
 
 /**
